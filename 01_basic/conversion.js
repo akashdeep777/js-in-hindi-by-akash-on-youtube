9c1f0961d585d309801eba3e3b6136f7893dbf53
akashdeep777 <168394522+akashdeep777@users.noreply.github.com>
2024-04-29 13:09:18 +0000

    conversion of datatypes

---
+++ 01_basic/conversion.js
@@ -0,0 +1,29 @@
+let score = "aa20";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInnumber = Number(score)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+// "33" => 33
+// "33abc" => nan
+// true => 1 ; false => 0;
+
+let islogged = 1
+// let islogged = "hello"
+
+let booleanisloged = Boolean(islogged);
+console.log(booleanisloged);
+console.log(typeof booleanisloged);
+
+// 1 => true   
+// 0 => false
+// "hello" => true
+
+let num = 222;
+
+let convertintostring = String(num);
+console.log(convertintostring);
+console.log(typeof convertintostring);
