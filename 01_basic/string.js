9c906031afdc3a9c17df7ef3a246379b50af550b
akashdeep777 <168394522+akashdeep777@users.noreply.github.com>
2024-05-01 07:23:12 +0000

    string summery and string function methods

---
+++ 01_basic/string.js
@@ -0,0 +1,28 @@
+let str = new String("akash - deep");
+
+// console.log(str)
+// console.log(str.charAt(2))
+// console.log(str.toUpperCase())
+// console.log(str[3])
+// console.log(str.__proto__)
+// console.log(str.indexOf("k"))
+
+// const newstr = str.substring(0 ,3);
+// console.log(newstr)
+// const onothernewstr = str.slice(-5 ,1);
+// console.log(onothernewstr)
+
+const myname = "akash deep";
+
+// console.log(myname);
+// console.log(myname.trim());
+// console.log(Array.from(str))
+// console.log(typeof str);
+
+const newstr = str.split("-");
+// console.log(newstr);
+
+const url = "http::/helloakash@gmaicl.com"
+// console.log(url.replace("c" , ""));
+// console.log(url.includes("akash"));
+
