bb6d4979f9288ec1bcae16310b0ff497bdbdb67d
akashdeep777 <168394522+akashdeep777@users.noreply.github.com>
2024-05-01 03:35:59 +0000

    datatype summery

---
+++ 01_basic/datatype.js
@@ -0,0 +1,23 @@
+ // <----------primitive----------> 
+ 
+ // 7 types : string,number,boolean,null,undefined,bigint,symbol;
+
+ const id = Symbol("1234")
+ const onotherid = Symbol("1234")
+ 
+ console.log(id);
+ console.log(onotherid);
+ console.log(typeof onotherid);
+ console.log(id === onotherid);
+ 
+ const bignumber = 3892474988499n
+ 
+ console.log(bignumber);
+ console.log(bignumber === BigInt);
+ // <----------end of non primitive----------> 
+
+  // reference (non primitive)
+ 
+ // array , objects , function;
+ 
+ // <----------end of non primitive----------> 
