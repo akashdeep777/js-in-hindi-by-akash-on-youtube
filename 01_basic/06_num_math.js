9f8ecea3678f66daf5b81c80d15514014cd38c24
akashdeep777 <168394522+akashdeep777@users.noreply.github.com>
2024-05-02 07:01:45 +0000

    math and numbers

---
+++ 01_basic/06_num_math.js
@@ -0,0 +1,39 @@
+// const score = 100;
+
+// console.log(score);
+// console.log(typeof score);
+
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(typeof balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+// console.log(typeof balance);
+
+// const number = 12.897;
+// const numbertwo = 212.897;
+
+// console.log(number.toPrecision(2));
+// console.log(numbertwo.toPrecision(2));
+
+// const hundred = 1000000;
+
+// console.log(hundred.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++ MATH ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.round((Math.random() * 10) + 1));
+// console.log(Math.pow(22,2));
+
+// const digit = ((Math.random() * 10) + 1);
+// console.log(digit.toPrecision(1))
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
