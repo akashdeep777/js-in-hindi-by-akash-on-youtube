8b291756a9f4ed4cd753251c5cd1a7894482aae4
akashdeep777 <168394522+akashdeep777@users.noreply.github.com>
2024-05-06 04:23:52 +0000

    scope

---
+++ 01_basic/scope.js
@@ -0,0 +1,39 @@
+
+function one() {
+    const username = "akash"
+
+    function two() {
+        const secname = "rahul";
+        // console.log(username)
+    }
+
+    // console.log(secname)
+
+    // two()
+}
+
+// console.log(one());
+
+if (true) {
+    const username = "rahul"
+    if (username === "rahul") {
+        const website = " hellowevdeb"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// +++++++++++++++++++++++++Intersting++++++++++++++++++
+
+one(5)
+function one(num) {
+    return num + 1
+}
+
+
+two(6)
+const two = function (num) {
+    return num + 1
+}
