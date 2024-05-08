18f321cd2c2813ad7e44bfe034020adff18a3978
akashdeep777 <168394522+akashdeep777@users.noreply.github.com>
2024-05-08 04:20:19 +0000

    for loops

---
+++ 05_itiration/01_one.js
@@ -0,0 +1,47 @@
+// for
+
+for (let index = 1; index <= 10; index++) {
+    const element = index;
+    if (element == 5) {
+        // console.log("5 is best number");
+    }
+    // console.log(element);
+}
+
+// console.log(element);
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop ${i}`);
+//     for (let j = 1; j <= 10 ; j++) {
+//         console.log( i + `*` + j + ` = ` + i*j );
+//         // console.log(`inner loop value  is ${j} and outer is ${i}`);
+
+//          }
+
+// }
+
+// let myArray = ['flash' , 'batman' , 'superman']
+// // console.log(myArray.length);
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     // console.log(element);
+// }
+
+// BREAK AND CONTINUE
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`five is detected`);
+//         break
+//     }
+//     console.log(`value of i is ${index}`);
+// }
+
+for (let index = 1; index <= 10; index++) {
+    if (index == 5) {
+        console.log(`five is detected`);
+        continue
+    }
+    console.log(`value of i is ${index}`);
+}
